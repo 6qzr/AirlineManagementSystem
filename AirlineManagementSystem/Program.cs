53300969c5055cf00d096da7ee72923262f0a7b9
Mohammed Al Abri <striker.m901@gmail.com>
2026-05-18 11:22:14 +0400

    Define all enums

--- AirlineManagementSystem/Program.cs
+++ AirlineManagementSystem/Program.cs
@@ -1,5 +1,18 @@
 ﻿namespace AirlineManagementSystem
 {
+    // Define all enums (based on attributes with multible fixed values)
+    enum AircraftStatus {Active, UnderMaintenance, Retired}
+    enum FlightStatus {Scheduled, Boarding, Departed, Arrived, Delayed, Cancelled}
+    enum LoyaltyTier { Bronze, Silver, Gold, Platinum}
+    enum CrewMemberRole {Pilot, CoPilot, CabinCrew, GroundStaff}
+    enum TicketSeatClass {Business, Economy}
+    enum TicketStatus {Confirmed, Cancelled, CheckedIn, Boarded}
+    enum BaggageType {Cabin, Hold, Oversized}
+    enum BaggageStatus {CheckedIn, Loaded, Lost, Delivered}
+    enum PromotionApplicableClass {Economy, Business, Both}
+
+    
+
     internal class Program
     {
         static void Main(string[] args)
